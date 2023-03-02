3b7ad36a80053bd3d64c55f04d1994a8afcd5d58
Joey-Li-emse <joey.li@etu.emse.fr>
2023-03-02 10:47:59 +0100

    clippy fix

--- src/commands.rs
+++ src/commands.rs
@@ -24,13 +24,7 @@ pub fn encode(args: &args::EncodeArgs)
 
     let final_content = png.as_bytes(); 
 
-    let _result = fs::write(&fp, final_content).unwrap(); 
-
-    println!("We encode with : 
-    file path : {:?}
-    chunk type : {:?}
-    message {:?}",
-            fp, chunkt_str, msg);
+    fs::write(&fp, final_content).unwrap(); 
 }
 
 pub fn decode(args: &args::DecodeArgs)
@@ -39,7 +33,7 @@ pub fn decode(args: &args::DecodeArgs)
     let chunkt_str = args.chunk_type.clone();
     let chunkt = ChunkType::from_str(&chunkt_str).unwrap(); 
 
-    let content = fs::read(&fp).unwrap();
+    let content = fs::read(fp).unwrap();
     let png = Png::try_from(&content[..]).unwrap(); 
 
     let chunkl = png.chunks(); 
@@ -65,15 +59,11 @@ pub fn remove(args: &args::RemoveArgs)
     let content = fs::read(&fp).unwrap();
     let mut png = Png::try_from(&content[..]).unwrap(); 
 
-    let result = png.remove_chunk(&String::from_utf8(chunkt.bytes().to_vec()).unwrap()).unwrap();
+    png.remove_chunk(&String::from_utf8(chunkt.bytes().to_vec()).unwrap()).unwrap();
 
     let final_content = png.as_bytes(); 
 
-    let _result = fs::write(&fp, final_content).unwrap(); 
-
-
-    println!("chunk {:?} removec", result); 
-
+    fs::write(&fp, final_content).unwrap(); 
 }
 
 pub fn print(args: &args::PrintArgs)
@@ -81,7 +71,7 @@ pub fn print(args: &args::PrintArgs)
     let fp = args.file_path.clone(); 
     let chunkt = ChunkType::from_str("dEAD").unwrap(); 
 
-    let content = fs::read(&fp).unwrap();
+    let content = fs::read(fp).unwrap();
     let png = Png::try_from(&content[..]).unwrap(); 
 
     let chunkl = png.chunks(); 
